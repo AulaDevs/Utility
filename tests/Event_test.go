d179a0fde6715c84dfc6196a161d6c6deb03a3e5
Hadaward <eduardo.gimenez07@gmail.com>
2023-08-05 21:51:21 -0300

    fix test event

--- tests/Event_test.go
+++ tests/Event_test.go
@@ -10,21 +10,13 @@ var handler *EventHandler = NewEventHandler()
 
 func TestEventHandler(t *testing.T) {
 	canExit := make(chan bool)
-	canProcess := make(chan bool)
 
 	go func() {
-		go func() {
-			canProcess <- true
-		}()
 		args := handler.ListenAndWait("test")
-		a := args[0].(int)
-		b := args[1].(string)
-		t.Logf("Event 'test' called with args: (%d, %s)", a, b)
-
+		t.Logf("Event 'test' called with args: %v", args)
 		canExit <- true
 	}()
 
-	<-canProcess
 	t.Log("Running 'test' Emitter")
 	handler.Emit("test", Event{3, "Lucas"})
 
